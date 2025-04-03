d262d947c94893e418ddc6669e6440cd43c87b46
Henrique Zangaro <henrique.eng.prog@gmail.com>
2025-04-03 14:52:48 -0300

    IA

--- app/main.py
+++ app/main.py
@@ -25,6 +25,6 @@ async def read_thread(whatsapp_number: str):
 async def create_new_thread(whatsapp_number: str):
     thread = await get_thread_by_number(whatsapp_number)
     if thread:
-        return thread and {"message": "Thread already exist"}
+        return thread
     await create_thread(whatsapp_number)
     return {"message": "Thread created successfully"}
